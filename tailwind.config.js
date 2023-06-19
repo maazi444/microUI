9d3e0ddb4b0e193c1da32349440682cf42744cea
maazi444 <maazahmad915@gmail.com>
2023-06-19 09:38:28 +0500

    tailwind config added

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        brunoace: ["Bruno Ace SC", "cursive"],
+        courierprime: ["Courier Prime", "monospace"],
+        lucida: ["Lucida Console", "sans-serif"],
+      },
+      colors: {
+        blackish: "#212427",
+        greyish: "#f1f1f1",
+        lightGreyish: "#b3adad",
+      },
+      boxShadow: {
+        hardsm: "-10px 13px 0px rgba(0, 0, 0, 0.3)",
+        hardmd: "-15px 20px 0px rgba(0, 0, 0, 0.3)",
+        harddull: "-5px 5px 0px rgba(0, 0, 0, 0.8)",
+      },
+    },
+  },
+};
